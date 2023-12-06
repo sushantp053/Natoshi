8b4d4cfe68c7a4e8fd06e9d6b477192ec19c424c
Sushant <sushantp053@gmail.com>
2023-12-06 12:30:18 +0530

    Map with shape data

--- home/views.py
+++ home/views.py
@@ -8,6 +8,8 @@ from django.core.serializers import serialize
 def home(request):
     agriland = AgriLand.objects.all()
     d = serialize("geojson", AgriLand.objects.all())
-    context = {"agriland": d}
+    data = {"data": d}
+    v = AgriLand.objects.distinct("village").all()
+    c = AgriLand.objects.distinct("crop").all()
 
-    return render(request, "home.html", context=context)
+    return render(request, "home.html", context={"data": data, "villages": v, "crops": c})
--- static/main.js
+++ static/main.js
@@ -1,5 +1,7 @@
 lat = "17.31272258513325";
 lng = "73.88772606233213";
+
+//73.84776677812441, 17.3049261491680
 var map = L.map("map", {
   center: [lat, lng],
   zoom: 13,
@@ -28,8 +30,21 @@ L.marker([17.337664, 73.882345]).addTo(map);
 const agriland = JSON.parse(
   document.getElementById("agriland-data").textContent
 );
+// console.log(agriland);
+var natoshiData = JSON.parse(agriland.data);
+console.log(natoshiData);
 
-console.log(agriland);
+for (let i = 0; i < natoshiData.features.length; i++) {
+  const element = natoshiData.features[i];
+  coordinates = element.geometry.coordinates;
+  let latlngs = [];
+
+  for (let j = 0; j < coordinates[0][0].length; j++) {
+    let a = [coordinates[0][0][j][1], coordinates[0][0][j][0]];
+    latlngs.push(a);
+  }
+  L.polygon([latlngs], { color: "red" }).addTo(map);
+}
 
 p.bindPopup("I am a popup.");
 p.on("click", function (e) {
