fd40c0ac8b5269f5a0d3e6f10ea7bbc1d064e4c1
Sushant <sushantp053@gmail.com>
2023-12-07 12:39:15 +0530

    Filter working

--- static/main.js
+++ static/main.js
@@ -24,9 +24,6 @@ L.tileLayer("https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png", {
     'Map <a href="https://memomaps.de/">memomaps.de</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-var p = L.marker([lat, lng]).addTo(map);
-L.marker([17.337664, 73.882345]).addTo(map);
-
 const agriland = JSON.parse(
   document.getElementById("agriland-data").textContent
 );
@@ -34,23 +31,88 @@ const agriland = JSON.parse(
 var natoshiData = JSON.parse(agriland.data);
 console.log(natoshiData);
 
-for (let i = 0; i < natoshiData.features.length; i++) {
-  const element = natoshiData.features[i];
-  coordinates = element.geometry.coordinates;
-  let latlngs = [];
-
-  for (let j = 0; j < coordinates[0][0].length; j++) {
-    let a = [coordinates[0][0][j][1], coordinates[0][0][j][0]];
-    latlngs.push(a);
+var rice = L.layerGroup();
+var sugar = L.layerGroup();
+var barren = L.layerGroup();
+var groundnut = L.layerGroup();
+try {
+  for (let i = 0; i < natoshiData.features.length; i++) {
+    const element = natoshiData.features[i];
+    coordinates = element.geometry.coordinates;
+    let latlngs = [];
+    for (let j = 0; j < coordinates[0][0].length; j++) {
+      let a = [coordinates[0][0][j][1], coordinates[0][0][j][0]];
+      latlngs.push(a);
+    }
+    if (element.properties.crop == "Rice") {
+      // L.polygon([latlngs], { color: "green" }).addTo(map);
+      rice.addLayer(
+        L.polygon([latlngs], { color: "green", weight: 1, fillColor: "green" })
+      );
+    } else if (element.properties.crop == "Wheat") {
+      L.polygon([latlngs], { color: "blue" }).addTo(map);
+    } else if (element.properties.crop == "Sugarcane") {
+      sugar.addLayer(
+        L.polygon([latlngs], { color: "brown", weight: 1, fillColor: "green" })
+      );
+      // L.polygon([latlngs], { color: "red" }).addTo(map);
+    } else if (element.properties.crop == "Barren") {
+      barren.addLayer(
+        L.polygon([latlngs], { color: "grey", weight: 1, fillColor: "green" })
+      );
+    } else if (element.properties.crop == "Groundnut") {
+      groundnut.addLayer(
+        L.polygon([latlngs], { color: "yellow", weight: 1, fillColor: "green" })
+      );
+    }
   }
-  L.polygon([latlngs], { color: "red" }).addTo(map);
+} catch (err) {
+  console.log(err);
 }
+rice.addTo(map);
+sugar.addTo(map);
+barren.addTo(map);
+groundnut.addTo(map);
 
-p.bindPopup("I am a popup.");
-p.on("click", function (e) {
-  console.log("You clicked the map at " + e.latlng);
-});
+// p.bindPopup("I am a popup.");
+// p.on("click", function (e) {
+//   console.log("You clicked the map at " + e.latlng);
+// });
 
-map.on("click", function (e) {
-  console.log("You clicked the map at " + e.latlng);
-});
+// map.on("click", function (e) {
+//   console.log("You clicked the map at " + e.latlng);
+// });
+
+function changeCrop(crop) {
+  if (crop == "all") {
+    rice.addTo(map);
+    sugar.addTo(map);
+    barren.addTo(map);
+    groundnut.addTo(map);
+    return;
+  } else if (crop == "Rice") {
+    map.removeLayer(sugar);
+    map.removeLayer(barren);
+    map.removeLayer(groundnut);
+    map.removeLayer(rice);
+    rice.addTo(map);
+  } else if (crop == "Sugarcane") {
+    map.removeLayer(rice);
+    map.removeLayer(barren);
+    map.removeLayer(groundnut);
+    map.removeLayer(sugar);
+    sugar.addTo(map);
+  } else if (crop == "Barren") {
+    map.removeLayer(sugar);
+    map.removeLayer(rice);
+    map.removeLayer(groundnut);
+    map.removeLayer(barren);
+    barren.addTo(map);
+  } else if (crop == "Groundnut") {
+    map.removeLayer(sugar);
+    map.removeLayer(barren);
+    map.removeLayer(rice);
+    map.removeLayer(groundnut);
+    groundnut.addTo(map);
+  }
+}
